e08ee41c2f12ee4c800e7f8589096fd0d5add746
pozyanime <pozyanime@gmail.com>
2026-03-30 20:09:22 +0430

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "Downloading ttyd..."
 
-wget https://github.com/tsl0922/ttyd/releases/download/1.7.7/ttyd.x86_64 -O ttyd
+curl -L https://github.com/tsl0922/ttyd/releases/download/1.7.7/ttyd.x86_64 -o ttyd
 
 chmod +x ttyd
 
