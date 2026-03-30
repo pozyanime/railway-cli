42e591335df9ffd21d847f9341dff6b660244a08
pozyanime <pozyanime@gmail.com>
2026-03-30 20:17:49 +0430

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,17 +3,19 @@
 set -e
 
 echo "Installing curl..."
-
 apt update && apt install -y curl
 
 echo "Downloading ttyd..."
 
-curl -L https://github.com/tsl0922/ttyd/releases/download/1.7.7/ttyd.x86_64 -o ttyd
+curl -L -o ttyd https://github.com/tsl0922/ttyd/releases/download/1.7.7/ttyd.x86_64
 
 chmod +x ttyd
+
+echo "Checking ttyd..."
+ls -la ttyd
 
 PORT=${PORT:-8080}
 
 echo "Starting ttyd on port $PORT"
 
-./ttyd -p $PORT bash
+./ttyd -p $PORT -c admin:admin bash
